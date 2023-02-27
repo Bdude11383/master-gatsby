3786c7d66870a6764c4d34bd24692daaf0df66a5
Brian Kerschner <kerschner.brian@gmail.com>
2023-02-27 14:13:17 -0600

    getting image error

---
+++ gatsby/src/pages/beers.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function BeersPage() {
+  return (
+    <>
+      <p>Hey I'm the Beers page</p>
+    </>
+  );
+}
